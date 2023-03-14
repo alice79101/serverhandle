baa5f7abf49e6a6b69dd5dfdbc5a880d1b50f224
Mia <alice79101@gmail.com>
2023-03-14 11:03:28 +0800

    style: homepage & login routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::match(['get', 'post'], '/login', function() {
+    //...
+});
+Route::match(['get', 'post'], '/signup', function() {
+    //...
+});
+Route::get('/logout', function() {
+    //...
 });
