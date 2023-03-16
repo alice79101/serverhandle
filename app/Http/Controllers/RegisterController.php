1d7b018dc428d3b738dbc929329308011fe821a1
Mia <alice79101@gmail.com>
2023-03-16 11:56:56 +0800

    feat: 登入後顯示狀態、style: 調整layout.blade

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -15,12 +16,20 @@ class RegisterController extends Controller
     public function store()
     {
 //        return \request()->all();
-        User::create(request()->validate([
+        $attributes = request()->validate([
            'name' => 'required|max:255|min:3',
-           'email' => 'required|email|max:255|min:5',
+           'email' => 'required|email|min:5|max:255|unique:users,email',
+//           'email' => ['required', 'min:3', 'max:255', Rule::unique('users', 'email')->ignore()],
            'password' => 'required|max:255|min:6'
-        ]));
-//        dd('success');
-        return redirect('/');
+        ]);
+
+//        $attributes['password'] = bcrypt($attributes['password']); // 密碼 hash
+        $user = User::create($attributes);
+        auth()->login($user);
+
+
+//        session()->flash('success', 'Your account has been created.');
+//        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
