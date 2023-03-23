051ec25628319d6f48d591389a101bdede496144
Mia <alice79101@gmail.com>
2023-03-23 14:07:20 +0800

    feat: /api可取得所有User

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    return \App\Models\User::all();
+});
+Route::get('/', function () {
+    return User::all();
+});
+
